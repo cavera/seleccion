102cc05b6e1df63871410e16c266eacbbe253dcb
Leonardo <cavera.de@gmail.com>
2013-06-26 22:41:56 -0500

    ajustes adicionales
    aumento en las gráficas

---
+++ js/behav.js
@@ -0,0 +1,66 @@
+$(function() {
+	$('.infoIndicador').hide();
+	$('.infoIndicador:nth-child(1)').fadeIn(500);
+	$('.infoIndicador:nth-child(2)').delay(100).fadeIn(500);
+	$('.infoIndicador:nth-child(3)').delay(200).fadeIn(500);
+
+
+// menu
+	$(".item").hover(
+		function  () {
+			$(this).find(".submenu1")
+				.slideDown("fast");
+		},
+		function  () {
+			$(this).find(".submenu1")
+				.slideUp("fast");
+		}
+	);
+// fecha
+	var $datePicker = $('input[name^="fecha"]');
+	$datePicker.datepicker({ dateFormat: "dd-mm-yy" });
+// precarga
+	var $precarga = $('.preload');
+
+	$precarga.progressbar({
+		value: 30
+	});
+
+	var checkCabeza = $('th');
+	function cambiarCabeza (elemento) {
+		elemento.change(
+		function  () {
+			if ($(this).is(':checked')) {
+				$(this).parent().addClass('th_selected');
+			}else{
+				$(this).parent().removeClass('th_selected');
+			}
+		});
+	}
+
+	cambiarCabeza(checkCabeza);
+
+// editar filas
+
+	var $checkFila = $('tbody input[type="checkbox"]');
+
+	function editarFila (elemento) {
+		elemento.change(
+		function(){
+			if($(this).is(':checked'))
+			{
+				$(this).closest('tr').find('td').addClass('editable');
+				$(this).closest('tr').find('td').attr('contenteditable','true');
+			}
+			else
+			{
+				$(this).closest('tr').find('td').removeClass('editable');
+				$(this).closest('tr').find('td').attr('contenteditable','false');
+			}
+			$(this).parent().attr('contenteditable','false');
+		}).change();
+	}
+
+	editarFila($checkFila);
+
+});
